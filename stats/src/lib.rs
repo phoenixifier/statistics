dc49596efbeec3a237cf2a4299185130f164661e
Vesonify <rkodiriy@students.wiut.uz>
2023-01-22 16:10:12 +0500

    Refactoring Formula

--- stats/src/lib.rs
+++ stats/src/lib.rs
@@ -1,15 +1,16 @@
 mod base;
 use sqrt::sqrt;
 use crate::base::factorial;
-pub struct Value{
+
+//For Binomial Distribution
+pub struct Binom{
     pub n: u32, // number of trials
     pub p: f64, // probability of success
     pub q: f64, // probability of failure
     pub x: u32, // number of success
 }
 
-//Mean, Variance and Standard Deviation Formula
-impl Value{
+impl Binom{
     pub fn mean(&self) -> f64{
         self.n as f64 * self.p
     }
@@ -26,5 +27,25 @@ impl Value{
         (factorial(self.n) as f64
         / ((factorial(self.n - self.x) as f64) * factorial(self.x) as f64))
         * ((self.p as u32 ^ self.x ) as f64 * (self.q as u32 ^ (self.n - self.x)) as f64)
+    }
+}
+
+//For Uniform Distributiion
+pub struct Uniform{
+    pub a: u32,
+    pub b: u32,
+}
+
+impl Uniform{
+    pub fn mean(&self) -> f64 {
+        ((self.a + self.b) / 2) as f64
+    }
+
+    pub fn fx(&self) -> f64 {
+        (1 / (self.b - self.a)) as f64
+    }
+
+    pub fn std_deviation(&self) -> f64 {
+        sqrt(self.b - self.a) ^ 2 / 12 as f64
     }
 }
--- stats/src/main.rs
+++ stats/src/main.rs
@@ -1,7 +1,7 @@
-use stats::Value;
+use stats::Binom;
 
 fn main() {
-    let num = Value {n: 5, p: 0.75, q: 0.25, x: 0};
+    let num = Binom {n: 5, p: 0.75, q: 0.25, x: 0};
     println!("{}", num.variance());
 }
 
